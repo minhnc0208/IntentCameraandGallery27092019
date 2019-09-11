09abca0d5c0f048883b547e5425762d5362c01ac
minhnc02082000 <53040823+minhnc02082000@users.noreply.github.com>
2019-09-11 18:04:34 +0700

    'A'

--- app/src/main/java/com/example/intentcameraandgallery29072019/MainActivity.java
+++ app/src/main/java/com/example/intentcameraandgallery29072019/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.intentcameraandgallery29072019;
 
 import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.Observer;
 
 public class MainActivity extends AppCompatActivity {
+
     Button btnCamera, btnGallery;
     ImageView img;
     int Request_Code_Camera = 1;
+    int Request_Code_Gallery = 2;
+    MainModel mainmodel = new MainModel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +31,54 @@ public class MainActivity extends AppCompatActivity {
         btnCamera = findViewById(R.id.buttonCamera);
         btnGallery = findViewById(R.id.buttonGallery);
         img = findViewById(R.id.imageview);
+
+        mainmodel.mimgHinh.observe(this, new Observer<Bitmap>() {
+            @Override
+            public void onChanged(Bitmap bitmap) {
+                if (bitmap != null) {
+                    img.setImageBitmap(bitmap);
+                }
+            }
+        });
+
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, Request_Code_Camera);
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        Request_Code_Camera);
             }
         });
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        Request_Code_Gallery);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == Request_Code_Camera) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, Request_Code_Camera);
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == Request_Code_Camera && resultCode == RESULT_OK && data != null) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            mainmodel.setImageBitmap(bitmap);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/example/intentcameraandgallery29072019/MainModel.java
+++ app/src/main/java/com/example/intentcameraandgallery29072019/MainModel.java
@@ -1,14 +1,20 @@
 package com.example.intentcameraandgallery29072019;
 
+import android.graphics.Bitmap;
+
 import androidx.lifecycle.LifecycleObserver;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainModel extends ViewModel implements LifecycleObserver {
-    public MainModel(){
 
+    public MainModel() {
     }
-//    LiveData<String> stringLiveData;
-    MutableLiveData<String> imgHinh = new MutableLiveData<>();
+
+    MutableLiveData<Bitmap> mimgHinh = new MutableLiveData<>();
+
+    public void setImageBitmap(Bitmap imageBitmap) {
+        mimgHinh.setValue(imageBitmap);
+    }
+
 }
